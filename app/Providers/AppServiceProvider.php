20e7d887bdfd48ead735a1fd9be657f393fd89f5
Leonardo Carvalho <leonardokarvalho@gmail.com>
2026-04-25 13:06:54 -0300

    preventLazyLoading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\{DB, Date};
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        Model::preventLazyLoading(!app()->isProduction());
     }
 
     /**
